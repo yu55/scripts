edbe262df1b3e360e516edcfdd45695c881ed866
Marcin P <marcin.pil@gmail.com>
2016-11-23 09:51:20 +0100

    Logging update

--- pi/relay/relay_by_temp.bash
+++ pi/relay/relay_by_temp.bash
@@ -19,7 +19,7 @@ if [ $CMP_RESULT -eq 1 ] && [[ "$LEVEL" == "0" ]]; then
     gpio -g mode 11 out
     gpio -g write 11 1
 
-    logger "T_ON=$T_ON, T_OFF=$T_OFF, T=$T Set relay to ON";
+    logger "$0: T_ON=$T_ON, T_OFF=$T_OFF, T=$T Set relay to ON";
 fi
 
 CMP_RESULT=`echo $T'<='$T_OFF | bc -l`;
@@ -28,19 +28,19 @@ if [ $CMP_RESULT -eq 1 ] && [[ "$LEVEL" == "1" ]]; then
     gpio -g mode 11 out
     gpio -g write 11 0
 
-    logger "T_ON=$T_ON, T_OFF=$T_OFF, T=$T Set relay to OFF";
+    logger "$0: T_ON=$T_ON, T_OFF=$T_OFF, T=$T Set relay to OFF";
 fi
 
 OLD_LEVEL=$LEVEL
 LEVEL=`gpio -g read 11`;
-logger "T_ON=$T_ON, T_OFF=$T_OFF, T=$T, OLD_LEVEL=$OLD_LEVEL, LEVEL=$LEVEL";
+logger "$0: T_ON=$T_ON, T_OFF=$T_OFF, T=$T, OLD_LEVEL=$OLD_LEVEL, LEVEL=$LEVEL";
 
 for i in 1 2 3
 do
     sqlite3 /var/local/relay-by-temp-db.sl3 "INSERT INTO pin11 VALUES(datetime(CURRENT_TIMESTAMP, 'localtime'), '$LEVEL');";
     LAST_RESULT=$?;
     if [ $LAST_RESULT -ne 0 ]; then
-        logger "Error while saving relay level to database: $LAST_RESULT";
+        logger "$0: Error while saving relay level to database: $LAST_RESULT";
         sleep 2;
     else
         break;
